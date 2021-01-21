e969ef527079538140a11fcaabb168274bcca45e
约坦 <mike.lx@alibaba-inc.com>
2021-01-21 16:32:50 +0800

    new code2

--- src/main/java/com/example/ci/HelloController.java
+++ src/main/java/com/example/ci/HelloController.java
@@ -12,16 +12,16 @@ public class HelloController{
 
 	@RequestMapping("/")
 	public String index(){
-		return "Hello World! mike";
+		return "Hello World! mike 16:32:31";
 	}
 
 	@RequestMapping("/hello")
 	public String hello(){
-		return "Hello World 1234!";
+		return "Hello World 1234!  16:32:31";
 	}
 
 	@RequestMapping("/from")
 	public String from(){
-		return "Hello World from!";
+		return "Hello World from!  16:32:31";
 	}
 }
